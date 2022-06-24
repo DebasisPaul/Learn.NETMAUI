3a8b1785fb6d7844f02e3d4dbb74b02ed75fec56
debas <debas@DEBASISPAUL>
2022-06-23 21:43:35 -0700

    Add project files.

---
+++ ShopOnline.Api/Repositories/Contarcts/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Repositories.Contarcts
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetItems();
+        Task<IEnumerable<ProductCategory>> GetCategories();
+        Task<Product> GetItems(int id);
+        Task<ProductCategory> GetCategory(int id);
+    }
+}
---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contarcts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+        public async Task<IEnumerable<ProductCategory>> GetCategories()
+        {
+            var categories = await this.shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        public Task<ProductCategory> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetItems()
+        {
+            var products = await this.shopOnlineDbContext.Products.ToListAsync();
+            return products;
+        }
+
+        public Task<Product> GetItems(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
